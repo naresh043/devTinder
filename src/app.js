48129929768555063da3bea67b9b3abdd74d37da
NareshSanjeev <naresh732003@gmail.com>
2025-06-23 16:51:13 +0530

    Refactor user routes in app.js and clean up commented code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express= require('express');
 const app = express();
-app.use("/test",(req,res)=>{
-    res.send("Hello from test route");
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello from Hello route");
-})
+// app.get("/user",(req,res)=>{
+//     res.send("Hello World");
+// });
+app.use("/user/abc/xyz",(req,res)=>{
+    res.send("Hello from abc/xyz");
+});
+
+app.use("/user/abc",(req,res)=>{
+    res.send("Hello from middleware");
+});
+app.use("/user",(req,res)=>{
+    res.send("Hello World");
+});
+
 app.use("/",(req,res)=>{
-    res.send("Hello from root route");
-})
+    res.send("Hello from root");
+});
 
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");

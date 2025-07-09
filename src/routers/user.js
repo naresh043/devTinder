816d30e0cb186ec74f685d0adcb52f1bec3d5099
NareshSanjeev <naresh732003@gmail.com>
2025-07-09 11:19:44 +0530

    Add feed endpoint to fetch users excluding hidden connections; improve logging

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/authMiddleware");
+const User = require("../models/user");
 const connectionRequestModel = require("../models/connectionRequest");
 
 const USER_SAFE_DATA = "firstName lastName age gender photoURL skills";
@@ -25,7 +26,7 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    console.log(loggedInUser,"log User")
+    console.log(loggedInUser, "log User");
     const connectionRequests = await connectionRequestModel
       .find({
         $or: [
@@ -35,7 +36,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       })
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
-      console.log(connectionRequests)
+    console.log(connectionRequests);
     const data = connectionRequests.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return row.toUserId;
@@ -49,6 +50,36 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   } catch (err) {
     console.error("Error: " + err.message);
     res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId toUserId");
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString()),
+        hideUserFromFeed.add(req.toUserId.toString());
+    });
+    const Users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    // console.log(Users);
+    res.send(Users);
+  } catch (err) {
+    console.error("ERROR: " + err.message);
+    res.status(400).json({
+      message: "ERROR: " + err.message,
+    });
   }
 });
 module.exports = {
